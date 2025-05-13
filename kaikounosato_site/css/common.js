de51b9746981aa75ec81e343f22e4aba2ee1fc5e
reon369 <tokutyaiiii@gmail.com>
2025-05-13 16:47:34 +0900

    5/13

--- kaikounosato_site/css/common.js
+++ kaikounosato_site/css/common.js
@@ -1,60 +1,57 @@
-$(function() {
+$(function () {
+  // ドロワー
   $('.drawer-item>a').click(function() {
     $('#drawer-check').removeAttr('checked').prop('checked', false).change();
   });
-});
-$(".slick01").slick({
+
+  // slick
+  $(".slick01").slick({
     dots: true,
-	autoplay: true,
+    autoplay: true,
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-	dotsClass: "slide-dots" //ここでclass名を変更する（デフォルトはslick-dots）
-  });
-$(function () {
-  $("slick01").slick({
     slidesToShow: 4,
+    slidesToScroll: 3,
+    dotsClass: "slide-dots",
     responsive: [
       {
-        breakpoint: 1024, // 768〜1023px以下のサイズに適用
+        breakpoint: 1024,
         settings: {
           slidesToShow: 3,
         },
       },
       {
-        breakpoint: 768, // 480〜767px以下のサイズに適用
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
         },
       },
       {
-        breakpoint: 480, // 〜479px以下のサイズに適用
+        breakpoint: 480,
         settings: {
           slidesToShow: 1,
         },
       },
     ],
   });
-});
 
-
-  // ふわっ
- function fadeAnime(){
-
-  // ふわっ
-  $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
-    var elemPos = $(this).offset().top-20;//要素より、50px上の
-    var scroll = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    if (scroll >= elemPos - windowHeight){
-    $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
-    }else{
-    $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
-    }
+  // fadeUp
+  function fadeAnime() {
+    $('.fadeUpTrigger').each(function () {
+      var elemPos = $(this).offset().top - 20;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight) {
+        $(this).addClass('fadeUp');
+      } else {
+        $(this).removeClass('fadeUp');
+      }
     });
-}
+  }
 
-// 画面をスクロールをしたら動かしたい場合の記述
-  $(window).scroll(function (){
-    fadeAnime();/* アニメーション用の関数を呼ぶ*/
-  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function () {
+    fadeAnime();
+  });
+
+  // 初回実行
+  fadeAnime();
+});
